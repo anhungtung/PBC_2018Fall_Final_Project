2ace284bda897f428aab1d33365dc47fcc53c023
Edward Tung <43094164+anhungtung@users.noreply.github.com>
2018-12-09 23:09:59 +0800

    Update and rename __n.py to __init__.py

--- mixer/__init__.py
+++ mixer/__init__.py
@@ -1,7 +1,5 @@
 import logging
 import argparse
-from splinter import *
-from visualizer import *
 
 logging.basicConfig(level=logging.DEBUG, format="[%(levelname)-0s] %(name)-0s >> %(message)-0s")
 logger = logging.getLogger(__name__)
