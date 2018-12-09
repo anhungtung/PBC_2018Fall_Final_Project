533468256f3ed248ff3e9750028da8a2865c8312
Edward Tung <43094164+anhungtung@users.noreply.github.com>
2018-12-09 23:08:52 +0800

    Update run.py

--- run.py
+++ run.py
@@ -14,18 +14,15 @@ formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
 console.setFormatter(formatter)
 logging.getLogger('').addHandler(console)
 
-if __name__ == "__main__":
-    arg_model = sys.argv[sys.argv.index("--model") + 1]
+def logger_config(debug, to_file=False):
+    if debug:
+        logging.config.dictConfig(dict_config)
+        logging.info('Debug mode.')
+    else:
+        if to_file:
+            dict_config['loggers']['mixer_music'] = ['player']
+        else:
+            dict_config['loggers']['mixer_music'] = ['music_mixer']
 
-    parser = argparse.ArgumentParser(prog = 'Music_Mixer', epilog = 'Splinder Music Mixer and Pygame Dynamic GUI')
-    parser.add_argument('--mixer', help = 'display mixer gui')
-    parser.add_argument('--player', help = 'display mp3 player')
-
-    args = parser.parse_args()
-
-    if args.mixer :
-        # Code Here
-    elif args.player:
-        # Code Here
-
-    logger.debug(args)
+        logging.config.dictConfig(dict_config)
+        logging.info('Release mode.')
