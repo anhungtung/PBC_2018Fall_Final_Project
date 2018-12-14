f678031565d517216804823a89f493d06ac54b5a
Edward Tung <43094164+anhungtung@users.noreply.github.com>
2018-12-14 22:39:20 +0800

    Create webscraping.py

---
+++ backend/webscraping.py
@@ -0,0 +1,33 @@
+import beatifulsoup as bs4
+import abc
+import os, shutil
+
+class DatabaseManager(Metaclass = abc.abstractmethod):
+   def __init__(self):
+    pass
+
+   def directory():
+    #Coding Here
+    return direc
+   
+   @abc.abstractproperty
+   @staticmethod
+   @abc.abstractmethod
+   def database_update(method = False):
+    os.chdir('/home')
+    url = 'ssss.com'
+    if method == True:
+      shutil.clean_folder
+    # Coding here
+    print('Database Reloading)
+    return None
+   
+   @staticmethod
+   def database_check():
+    if os.listdir('/home') != None:
+      # Check if every data exists
+    else:
+      database_update()
+      return None
+    return lost_data
+   
