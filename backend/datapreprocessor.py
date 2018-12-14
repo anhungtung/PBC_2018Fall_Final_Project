a63b98b28eaab49d44679457b60336124987da0d
Edward Tung <43094164+anhungtung@users.noreply.github.com>
2018-12-14 22:43:57 +0800

    Create datapreprocessor.py

---
+++ backend/datapreprocessor.py
@@ -0,0 +1,11 @@
+import logger
+
+logging.basicConfig(level=logging.DEBUG, format="[%(levelname)-0s] %(name)-0s >> %(message)-0s")
+logger = logging.getLogger(__name__)
+
+class Exec:
+  def __init__(self):
+    pass
+    
+  def exec():
+    #coding_here
