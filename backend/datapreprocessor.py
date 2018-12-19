f6d5ceecc75db8ff7276ae60147d734b455f9a8c
Edward Tung <43094164+anhungtung@users.noreply.github.com>
2018-12-19 22:19:19 +0800

    Update datapreprocessor.py

--- backend/datapreprocessor.py
+++ backend/datapreprocessor.py
@@ -1,4 +1,5 @@
 import logger
+from database import Get_Data
 
 logging.basicConfig(level=logging.DEBUG, format="[%(levelname)-0s] %(name)-0s >> %(message)-0s")
 logger = logging.getLogger(__name__)
@@ -8,4 +9,7 @@ class Exec:
     pass
     
   def exec():
+    table = pd.read_csv('......')
+   
+    table['log'] = Get_Data.log_changer(table['address'])
     #coding_here
