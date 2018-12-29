28bd7b7e75d3951969073c22251d9ac24c5112ce
Yvonn3 <45348051+Yvonn3@users.noreply.github.com>
2018-12-29 22:10:29 +0800

    Update datapreprocessor.py

--- backend/datapreprocessor.py
+++ backend/datapreprocessor.py
@@ -5,11 +5,32 @@ logging.basicConfig(level=logging.DEBUG, format="[%(levelname)-0s] %(name)-0s >>
 logger = logging.getLogger(__name__)
 
 class Exec:
-  def __init__(self):
+    def __init__(self):
     pass
     
-  def exec():
-    table = pd.read_csv('......')
+    def exec():
+        table = pd.read_csv('......')
+        table.columns=["number",'Reason','address','time']
+
+        table["type"] = np.nan 
+        table["point"] = np.nan
+    def cal_type(row):
+        if "清" in row["Reason"]:
+            row['type'] = "衛生"
+            row['point'] = 1
+
+        elif "噪音"in  row["Reason"]:
+            row['type'] = "住宅安寧"  
+            row['point'] = 2     
+        elif "海砂屋"in row["Reason"]:
+            row['type'] = "住宅安全"    
+            row['point'] = 1 
+        else:
+            row['type'] = "交通安全"  
+            row['point'] = True
+
+        return row
+    table = table.apply(cal_type, axis=1)
    
     table['log'] = Get_Data.log_changer(table['address'])
     #coding_here
