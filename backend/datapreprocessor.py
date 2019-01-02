595de7c7015a3e125e8a6a99d69368cd03a6f8bd
Yvonn3 <45348051+Yvonn3@users.noreply.github.com>
2019-01-02 23:04:40 +0800

    Update datapreprocessor.py

--- backend/datapreprocessor.py
+++ backend/datapreprocessor.py
@@ -3,32 +3,22 @@ from database import Get_Data
 
 logging.basicConfig(level=logging.DEBUG, format="[%(levelname)-0s] %(name)-0s >> %(message)-0s")
 logger = logging.getLogger(__name__)
-
+import pandas as pd
+import numpy as np
 class Exec:
     def __init__(self):
     pass
     
     def exec():
-        table = pd.read_csv('total_database.csv',encoding="big5")
+        table = pd.read_csv("1999_data.csv")#先指定第一行為indexcolumn
 
-        table["type"] = np.nan 
-        table["point"] = np.nan
-    def cal_type(row):
-        if "清" in row["派工項目"]:
-            row['type'] = "衛生"
-            row['point'] = 1
-
-        elif "噪音"in  row["派工項目"]:
-            row['type'] = "住宅安寧"  
-            row['point'] = 2     
-        elif "海砂屋"in row["派工項目"]:
-            row['type'] = "住宅安全"    
-            row['point'] = True 
-        else:
-            row['type'] = "交通安全"  
-            row['point'] = 1
-
-        return row
-    table = table.apply(cal_type, axis=1)
+        table = table.rename(columns={'0':'經緯'})
+        table["longitude"] =np.nan
+        table["laitude"] = np.nan
+        table["longitude"] = table['經緯'].str.split(",").str.get(0).str.lstrip( "(" )
+        table["laitude"] = table['經緯'].str.split(",").str.get(1).str.rstrip( ")" )
+        #把經緯度分開欄位
+        table = table.to_csv("1999_data.csv")#儲存檔案
+        return table
    
   
