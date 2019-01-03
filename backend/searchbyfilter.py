8888eb7160c005f7a7cad74866eb92091afd8f4a
Wowlin0403 <45348036+Wowlin0403@users.noreply.github.com>
2019-01-03 19:01:14 +0800

    Update searchbyfilter.py

--- backend/searchbyfilter.py
+++ backend/searchbyfilter.py
@@ -1,14 +1,15 @@
 import logger
 from database import Get_data
 import math
-
+import pandas
 
 #'user' and 'target' must be a list that have two items which is "longtitude" and "latitude"
 def calculate_distance(user, target):
     #R -> radius of earth
-    #need Absolute
+    la1, long1 = [math.pi/180*x for x in user]
+    la2, long2 = [math.pi/180*x for x in target]
     R = 6371000
-    d = R * math.acos(math.sin(user[0])*math.sin(target[0]) + math.cos(user[0])*math.cos(target[0])*math.cos(target[1]-user[1]))
+    d = R * math.acos(math.sin(la1)*math.sin(la2) + math.cos(la1)*math.cos(la2)*math.cos(long2-long1))
     return d
 
 def search(address, radius=500):
@@ -17,25 +18,20 @@ def search(address, radius=500):
     #address -> coordinate
     coordinate = Get_data.log_changer(address)
 
-    #calaculate the address 500m edge
-    distance = radius*9/1000000
-    top = coordinate[0] + distance
-    bot = coordinate[0] - distance
-    east = coordinate[1] + distance
-    west = coordinate[1] - distance
-
-    #filter the database
-    table = table[(table["longtitude"].between(bot, top)) & (table["latitude"].between(west, east))]
-
     #calculate the distance and delete the coordinate that distance is larger than 'radius' and format to dictionary
-    drop_list = []
-    for i in range(table.count()):
-        target = [table['longtitude'][i], table['latitude'][i]]
-        if calculate_distance(coordinate, target) < radius:
-            drop_list.append(i)
-        else:
+    result = []
+    for i in list(table.index):
+        d = calculate_distance(coordinate, [table['latitude'][i], table['longitude'][i]])
+        if d > radius:
             pass
-    table = list(table.drop(drop_list).values())
+        else:
+            temp = {}
+            #temp = {'編號': i, '立案時間': table['立案時間'][i], '派工項目': table['派工項目'][i], '案件地址': table['案件地址'], '經緯度': table['經緯']}
+            temp['編號'] = i
+            temp['立案時間'] = table['立案時間'][i]
+            temp['派工項目'] = table['派工項目'][i]
+            temp['案件地址'] = table['案件地址'][i]
+            temp['經緯度'] = table['經緯'][i]
+            result.append(temp)
 
-    return table
-            
+    return result
